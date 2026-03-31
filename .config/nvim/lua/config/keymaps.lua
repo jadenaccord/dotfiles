3a16d7b4ba132cc119da0552052b29be7420f62c
Jaden Accord <dev@jadenaccord.com>
2026-03-31 17:23:40 +0200

    remove lazy.nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,18 +1,19 @@
+local map = vim.api.nvim_set_keymap
+
 -- Map leader keys
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 
--- Leader keymaps
-vim.api.nvim_set_keymap('n', '<Leader>w', ':w<CR>', { noremap = true, silent = false }) -- save file with <space + w>
-vim.api.nvim_set_keymap('n', '<Leader>l', ':Lazy<CR>', { noremap = true, silent = true}) -- open lazy.nvim
-vim.api.nvim_set_keymap('n', '<Leader>m', ':Mason<CR>', { noremap = true, silent = true}) -- open mason.nvim
-vim.api.nvim_set_keymap('n', '<Leader>ff', ':Telescope find_files<CR>', { noremap = true, silent = true}) -- Telescope find_files
-vim.api.nvim_set_keymap('n', '<Leader>fg', ':Telescope live_grep<CR>', { noremap = true, silent = true}) -- Telescope live_grep
-vim.api.nvim_set_keymap('n', '<Leader>fb', ':Telescope buffers<CR>', { noremap = true, silent = true}) -- Telescope buffers
-vim.api.nvim_set_keymap('n', '<Leader>fh', ':Telescope help_tags<CR>', { noremap = true, silent = true}) -- Telescope help_tags
-vim.api.nvim_set_keymap('n', '<Leader>t', ':NvimTreeToggle<CR>', { noremap = true, silent = true}) -- toggle nvim-tree
+-- Saving, quitting, etc.
+map('n', '<Leader>w', ':w<CR>', { desc = 'Save file', nowait = true })
+map('n', '<Leader>rr', ':restart<CR>', { desc = 'Restart config' })
+map('n', '<Leader>qa', ':qa<CR>', { desc = 'Quit all' })
 
 -- Switch between buffers
-vim.api.nvim_set_keymap('n', '<Tab>', ':bnext<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<S-Tab>', ':bprev<CR>', { noremap = true, silent = true })
+map('n', '<Tab>', ':bnext<CR>', { silent = true, desc = 'Next buffer' })
+map('n', '<S-Tab>', ':bprev<CR>', { silent = true, desc = 'Previous buffer' })
+
+
+----------------------------------------------------------------------
+-- Note: plugin-specific keymaps are specified in the corresponding lua files.
 
