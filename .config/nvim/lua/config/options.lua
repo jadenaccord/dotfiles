8516e91730132d0cb202e5ce8ec4b83650d4572d
Jaden Accord <dev@jadenaccord.com>
2026-04-01 20:24:50 +0200

    update options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,24 +1,42 @@
 -- UI
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.wrap = true
+-- vim.o.wrap = true
 vim.o.linebreak = true
 vim.o.cursorline = true
 vim.o.splitbelow = true
 vim.o.splitright = true
 vim.o.termguicolors = false
+vim.o.scrolloff = 4
+vim.o.sidescrolloff = 4
+vim.o.signcolumn = "yes"
+-- vim.o.colorcolumn = "80"
+-- vim.api.nvim_set_hl(0, 'ColorColumn', { ctermbg = 7 })
+vim.o.showmatch = true
+vim.o.cmdheight = 1
+vim.o.showmode = false
 
 -- Tabs/indents
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.smartindent = true
+vim.o.autoindent = true
 
 -- Search
 vim.o.incsearch = true
-vim.o.hlsearch = true
+vim.o.hlsearch = false
 vim.o.ignorecase = true
 vim.o.smartcase = true
+
+-- Files
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.swapfile = false
+vim.o.undofile = true
+vim.o.undolevels = 10000
+vim.o.undodir = vim.fn.expand("~/.vim/undodir")
+vim.o.autoread = true
 
 -- Disable netrw (for nvim-tree)
 vim.g.loaded_netrw = 1
@@ -28,3 +46,6 @@ vim.g.loaded_netrwPlugin = 1
 vim.o.clipboard = 'unnamedplus'
 --vim.o.completeopt = {'menu', 'menuone', 'noselect'}
 vim.o.mouse = 'a'
+vim.o.updatetime = 300
+-- vim.o.timeoutlen = 500
+
