a4b399509c5b9d2b70fea96ebe05a3046f7cf1bf
Jaden Accord <dev@jadenaccord.com>
2026-04-09 18:56:50 +0200

    update nvim keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,4 @@
-local map = vim.api.nvim_set_keymap
+local map = vim.keymap.set
 
 -- Map leader keys
 vim.g.mapleader = ' '
@@ -13,6 +13,14 @@ map('n', '<Leader>qa', ':qa<CR>', { desc = 'Quit all' })
 map('n', '<Tab>', ':bnext<CR>', { silent = true, desc = 'Next buffer' })
 map('n', '<S-Tab>', ':bprev<CR>', { silent = true, desc = 'Previous buffer' })
 
+map('n', '<Leader>cv', ':e ~/.config/nvim/', { desc = 'Open nvim config' })
+map('n', '<Leader>cz', ':e $ZSHRC', { desc = 'Open .zshrc' })
+
+-- Visual <j> and <k>
+vim.keymap.set({'n', 'x'}, 'j', 'gj', { noremap = true, silent = true })
+vim.keymap.set({'n', 'x'}, 'k', 'gk', { noremap = true, silent = true })
+vim.keymap.set({'n', 'x'}, 'gj', 'j', { noremap = true, silent = true })
+vim.keymap.set({'n', 'x'}, 'gk', 'k', { noremap = true, silent = true })
 
 ----------------------------------------------------------------------
 -- Note: plugin-specific keymaps are specified in the corresponding lua files.
