c5db0270ee099cfddcd9dc4f342a8d2ea5b0fd27
Jaden Accord <dev@jadenaccord.com>
2026-04-09 23:32:50 +0200

    fix nvim-treesitter

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -5,7 +5,3 @@ vim.lsp.enable('html')
 vim.lsp.enable('cssls')
 vim.lsp.enable('ts_ls')
 
-vim.pack.add({ 'https://github.com/echasnovski/mini.nvim' })
-require('mini.completion').setup()
--- require('mini.snippets').setup()
-
--- .config/nvim/plugin/treesitter.lua
+++ .config/nvim/plugin/treesitter.lua
@@ -1,9 +1,15 @@
-vim.pack.add({ 'https://github.com/nvim-treesitter/nvim-treesitter' })
-
-require('nvim-treesitter.configs').setup({
-    highlight = { enable = true },
-    indent = { enable = true },
-    auto_install = true,
-    ensure_installed = { 'markdown', 'markdown_inline', 'lua', 'go', 'html', 'css', 'javascript' },
+vim.pack.add({
+    { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
 })
+
+require('nvim-treesitter.configs').setup {
+    ensure_installed = { 'html', 'css', 'javascript', 'typescript', 'markdown', 'markdown_inline', 'lua', 'c', 'go', 'vim', 'vimdoc' },
+    sync_install = false,
+    auto_install = true,
+    ignore_install = {},
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+}
 
